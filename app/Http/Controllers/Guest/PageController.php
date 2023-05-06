713315ca6dae3d7aa691618baae8e2e601bbe5aa
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-06 19:11:10 +0200

    query

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index() {
-        return view('home');
+        $trains = Train::all();
+
+        return view('home', compact('trains'));
     }
 }
