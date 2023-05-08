ae4359fc1ddd9992a0d6a1200e32700c4a06b07a
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-08 14:49:52 +0200

    carbon

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use App\Models\Train;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon as SupportCarbon;
 use Illuminate\Support\Facades\Date;
 
 class PageController extends Controller
 {
     public function index() {
+
         $trains = Train::all()
-            ->where('departure_time', '2023-05-06 17:18:32');
+            ->where('departure_time', '>=', SupportCarbon::today());
 
         return view('home', compact('trains'));
     }
